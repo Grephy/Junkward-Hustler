ae6395c0e3917321c898f9266320ffae7c1f93de
sahil <sahilmanjrekar2003@gmail.com>
2024-04-12 00:13:08 +0530

    login

--- js/main.js
+++ js/main.js
@@ -80,16 +80,7 @@
         dotsData: true,
     });
 
-    // Firebase configuration
-    const firebaseConfig = {
-        apiKey: "AIzaSyB7ZjbvzXGDo7gpSmzvbrNiNHeTiF1IOdE",
-        authDomain: "junkyard-hustlers.firebaseapp.com",
-        projectId: "junkyard-hustlers",
-        storageBucket: "junkyard-hustlers.appspot.com",
-        messagingSenderId: "577124411598",
-        appId: "1:577124411598:web:79a638875f7b0092c4889f",
-        measurementId: "G-7YHJH6CS6E"
-    };
+    
 
     // Initialize Firebase
     const app = firebase.initializeApp(firebaseConfig);
