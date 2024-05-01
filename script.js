c2b2ef44cbdda4b2d6bcaa84f110721b26eab401
AtharvaGandhi <gamagod124@gmail.com>
2024-05-01 12:47:56 +0530

    updated

--- script.js
+++ script.js
@@ -1,4 +1,25 @@
-function sendMail(){
+// function sendMail(){
+//     let parms = {
+//         name : document.getElementById("name").value,
+//         email : document.getElementById("email").value,
+//         pickup : document.getElementById("pickup").value,
+//         datee : document.getElementById("datee").value,
+//         timee : document.getElementById("timee").value,
+//         message : document.getElementById("message").value,
+//     }
+
+//     emailjs.send("service_v2zsfzw","template_bjc11cf",parms)
+//         .then(() => {
+//             alert("Appointment Booked!!");
+//         })
+//         .catch((error) => {
+//             alert("Error sending email:", error);
+//         });
+// }
+// //(alert("Appointment Booked!!"))
+function sendMail(event) {
+    event.preventDefault(); // Prevent default form submission
+
     let parms = {
         name : document.getElementById("name").value,
         email : document.getElementById("email").value,
@@ -13,7 +34,6 @@ function sendMail(){
             alert("Appointment Booked!!");
         })
         .catch((error) => {
-            alert("Error sending email:", error);
+            alert("Error sending email: " + error);
         });
 }
-//(alert("Appointment Booked!!"))
